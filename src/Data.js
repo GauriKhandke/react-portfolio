6668897d8c8bed9f1baf323c06e21c8822dabf36
Gauri Khandke <khandkegauri@gmail.com>
2020-12-18 18:35:39 -0800

    Added portfolio data

---
+++ src/Data.js
@@ -0,0 +1,84 @@
+import oasis from './assets/images/OasisLanding.png';
+import jobfinder from './assets/images/JobFinder.png';
+import burgerlogger from './assets/images/Burger-Logger.png';
+import googlebooks from './assets/images/GoogleBooksReact.png';
+import employeedirectory from './assets/images/EmployeeDirectory.png';
+import notetaker from './assets/images/NoteTaker.png';
+import weatherdashboard from './assets/images/weather-dashboard.png';
+import dayplanner from './assets/images/DayPlanner.png';
+import codequiz from './assets/images/codequiz.png';
+
+export const projects = [
+  {
+    id: "GauriKhandke/oasis",
+    title: "Oasis (MERN)",
+    description:
+      "Online journal Application that allows both young and young at heart to write their thoughts, feelings and emotions into a safe, secure and fun journal app. ",
+    imageUrl: oasis,
+    appUrl: "https://oasis-diary.herokuapp.com",
+  },
+  {
+    id: "GauriKhandke/google-books-search-react",
+    title: "Google Books Search (React)",
+    description:
+      "Google Books Search MERN app allow users to search for books via Google Books API and view/save books they wish to read. Also User can view/delete saved books.",
+    imageUrl: googlebooks,
+    appUrl: "https://google-books-search-app-mern.herokuapp.com/",
+  },
+  {
+    id: "GauriKhandke/JobFinder",
+    title: "Job Finder (Full Stack)",
+    description:
+      "This is a simple Job Finder application where the user can search for the Jobs with keywords like location,role etc.And also the user can Post a Job ",
+    imageUrl: jobfinder,
+    appUrl: "https://uw-jobfinder.herokuapp.com/",
+  },
+  {
+    id: "GauriKhandke/employee-directory",
+    title: "Employee Directory (React)",
+    description:
+      "Employee directory application created using react where user can view employee data, search employee or sort employee by name.",
+    imageUrl: employeedirectory,
+    appUrl: "https://gaurikhandke.github.io/employee-directory/",
+  },
+  {
+    id: "GauriKhandke/Burger-Logger",
+    title: "Burger Logger (Full Stack)",
+    description:
+      "This is a restaurant app where the users input the names of burgers  and has Devour button. When the user clicks it, the burger will be 'Devoured'.",
+    imageUrl: burgerlogger,
+    appUrl: "https://eat-da-burger-restaurant.herokuapp.com",
+  },
+  {
+    id: "GauriKhandke/Note-Taker",
+    title: "Note Taker (Full Stack)",
+    description:
+      "This is a simple Note Taker application that allows users to add ,view saved notes and also delete the notes if the user don't need that note anymore.",
+    imageUrl: notetaker,
+    appUrl: "https://gauri-express-notetaker.herokuapp.com/",
+  },
+  {
+    id: "GauriKhandke/Gauri-weather-dashboard",
+    title: "Weather Dashboard (FrontEnd)",
+    description:
+      "A weather dashboard application to get current weather conditions of city and 5 day forecast of that city.",
+    imageUrl: weatherdashboard,
+    appUrl: "https://gaurikhandke.github.io/Gauri-weather-dashboard/",
+  },
+  {
+    id: "GauriKhandke/Gauri-code-quiz",
+    title: "Code Quiz (Front End)",
+    description:
+      "A timer-based quiz application that stores high scores of multiple users. Answering incorrectly results in penality of time.",
+    imageUrl: codequiz,
+    appUrl: "https://gaurikhandke.github.io/Gauri-code-quiz/",
+  },
+  {
+    id: "GauriKhandke/Gauri-day-planner",
+    title: "Day Planner (Front End)",
+    description:
+      "Simple calendar application that allows the user to save events for each hour of the day from 9AM to 5 PM. ",
+    imageUrl: dayplanner,
+    appUrl: "https://gaurikhandke.github.io/Gauri-day-planner/",
+  },  
+];
