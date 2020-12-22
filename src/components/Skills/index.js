0618d8c9e0cb4830c97d8f51ed262462d48634a0
Gauri Khandke <khandkegauri@gmail.com>
2020-12-21 17:35:18 -0800

    Updated text color of skills

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -12,52 +12,52 @@ export default function Skills() {
                     <div className="col">
                         <div>
                             <i className="fab fa-html5 fa-3x" style= {{color: '#FF7F50'}}></i>
-                            <p>HTML5</p>
+                            <p style= {{color: 'white'}}>HTML5</p>
                         </div>
                     </div>
 
                     <div className="col">
                         <div >
                             <i className="fab fa-css3-alt fa-3x" style= {{color: '#1E90FF'}} ></i>
-                            <p>CSS3</p>
+                            <p style= {{color: 'white'}}>CSS3</p>
                         </div>
                     </div>
                     <div className="col">
                         <div >
                             <i className="fab fa-js-square fa-3x" style= {{color: '#DAA520'}}></i>
-                            <p>JavaScript</p>
+                            <p style= {{color: 'white'}}>JavaScript</p>
                         </div>
                     </div>
                     <div className="col">
                         <div >
                             <i className="fab fa-node fa-3x" style= {{color: '#9ACD32'}}></i>
-                            <p>NodeJS</p>
+                            <p style= {{color: 'white'}}>NodeJS</p>
                         </div>
                     </div>
                     <div className="col">
                         <div >
                             <i className="fab fa-react fa-3x" style= {{color: '#00CED1'}}></i>
-                            <p>React</p>
+                            <p style= {{color: 'white'}}>React</p>
                         </div>
                     </div>
                     <div className="col">
                         <div >
                             <i className="fas fa-database fa-3x" style= {{color: '#00758F'}}></i>
-                            <p>MySQL</p>
+                            <p style= {{color: 'white'}}>MySQL</p>
                         </div>
                     </div>
                     <div className="col">
                         <div >
                             <i className="fas fa-leaf fa-3x" style= {{color: '#4DB33D'}}></i>
-                            <p>MongoDB</p>
+                            <p style= {{color: 'white'}}>MongoDB</p>
                         </div>
                     </div>
                     
 
                     <div className="col">
                         <div >
-                            <i className="fab fa-github fa-3x"></i>
-                            <p>GitHub</p>
+                            <i className="fab fa-github fa-3x" style= {{color: 'white'}}></i>
+                            <p style= {{color: 'white'}}>GitHub</p>
                         </div>
                     </div>
                 </div>
