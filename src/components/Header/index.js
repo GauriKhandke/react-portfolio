cfeec18999fa4cc62a4986a292ea431ac1963564
Gauri Khandke <khandkegauri@gmail.com>
2020-12-23 15:27:06 -0800

    Updated tooltip for link icons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,7 +11,7 @@ export default function Header() {
         <div className ='container mb-3 mt-4' >
             <a href="https://github.com/GauriKhandke" className="btn tooltip-test icon" title="Github" target="_blank" rel="noopener noreferrer"
                 style={{fontSize:25, color:'#E8E8E8', marginRight:10}}><i className="fab fa-github"></i></a>
-            <a href="https://www.linkedin.com/in/gaurikhandke/" className="btn tooltip-test icon" title="linkedin"
+            <a href="https://www.linkedin.com/in/gaurikhandke/" className="btn tooltip-test icon" title="Linkedin"
                 target="_blank" rel="noopener noreferrer" style={{fontSize:25,color:'#E8E8E8',marginRight:10}}><i className="fab fa-linkedin"></i></a>
             <a href="mailto: khandkegauri@gmail.com" className="btn tooltip-test icon" title="Email" target="_blank" rel="noopener noreferrer"
                 style={{fontSize:25, color:'#E8E8E8', marginRight:10}}><i className="fas fa-envelope"></i></a>
