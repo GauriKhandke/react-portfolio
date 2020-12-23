38046999658099b7bc54b58e8472c2667056344b
Gauri Khandke <khandkegauri@gmail.com>
2020-12-22 22:51:46 -0800

    Added details button and modal for each project

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import { Card, Col } from 'react-bootstrap';
-
+import React, { useState } from 'react';
+import { Card, Col, Button, Modal } from 'react-bootstrap';
+import '../../assets/css/script.css';
+  
 export default function ProjectList(props) {
+
+  // Set show for modal
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
   return (
         <>
           <Col lg={4} style={{ padding: '20px',backgroundColor: 'transparent'}}>
@@ -10,13 +18,44 @@ export default function ProjectList(props) {
               <Card.Body style={{ paddingTop: '0'}}>
                 <Card.Title style={{fontSize:'1.3em'}}><strong>{props.project.title}</strong></Card.Title>
                 <Card.Text style={{fontSize:'1.2em', textAlign: 'left',color: '#444444',padding: '15px'}}>{props.project.description}</Card.Text>
-                <div >
-                  <a style={{ minWidth: '60px',margin: '15px 10px 5px',}} href={props.project.appUrl} target="_blank" rel="noopener noreferrer" >
-                    <i className='fab fa-chrome' style={{fontSize:'30px'}}></i>
-                  </a>
-                  <a style={{ minWidth: '60px',margin: '15px 10px 5px',}} href={'https://github.com/' + props.project.id} target="_blank" rel="noopener noreferrer">
-                    <i className='fab fa-github' style={{fontSize:'30px'}}></i>
-                  </a>
+                <div style={{ padding: '15px' }} >
+                  <div className="float-left">
+                  
+                  {/* Details Button */}
+									<Button	className="btnColor" variant='primary' onClick={ handleShow }	>
+									Details
+									</Button>
+
+                  {/* Details Modal */}
+                  <Modal show={show} onHide={handleClose} size = "lg">
+                    <Modal.Header closeButton>
+                      <Modal.Title className="modelHeading"><strong>{props.project.title}</strong></Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className = "d-flex justify-content-center" style={{marginBottom: '15'}}>
+                       
+                          <img src={props.project.gif} alt="Demo"
+                            class="img-thumbnail rounded float-left img-fluid d-flex"
+                          ></img>
+                        
+                        </div>
+                    </Modal.Body>
+                    <Modal.Body style={{ fontSize: '16px' }}>
+                      <h3>Description: </h3>
+                      <p>{props.project.details}</p>
+                      <h3>Technology Used: </h3>
+                      <p>{props.project.technology}</p>
+                    </Modal.Body>
+                  </Modal>
+                  </div>
+                  <div className="float-right">
+                    <a style={{ minWidth: '60px',margin: '15px 10px 5px',}} href={props.project.appUrl} target="_blank" rel="noopener noreferrer" >
+                      <i className='fab fa-chrome' style={{fontSize:'30px' }}></i>
+                    </a>
+                    <a style={{ minWidth: '60px',margin: '15px 10px 5px',}} href={'https://github.com/' + props.project.id} target="_blank" rel="noopener noreferrer">
+                      <i className='fab fa-github' style={{fontSize:'30px'}}></i>
+                    </a>
+                  </div>
                 </div>
               </Card.Body>
             </Card>
